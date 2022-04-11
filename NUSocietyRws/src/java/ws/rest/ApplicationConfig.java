0fc0fa25d476148fd0ff42e7c25860de7dd7435a
Sachin <e0540367@u.nus.edu>
2022-04-12 00:31:07 +0800

    Added event category stuff for REST

--- NUSocietyRws/src/java/ws/rest/ApplicationConfig.java
+++ NUSocietyRws/src/java/ws/rest/ApplicationConfig.java
@@ -31,6 +31,7 @@ public class ApplicationConfig extends Application {
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(ws.rest.CommentResource.class);
         resources.add(ws.rest.CorsFilter.class);
+        resources.add(ws.rest.EventCategoryResource.class);
         resources.add(ws.rest.PostResource.class);
         resources.add(ws.rest.StaffResource.class);
     }
